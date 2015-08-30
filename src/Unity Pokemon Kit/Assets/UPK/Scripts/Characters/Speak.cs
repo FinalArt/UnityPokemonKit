39510287b4a75ccdb2737e02187f909aa034d14e
FinalArt <finalart.mail@gmail.com>
2015-08-30 15:52:59 +0200

    Affichage d'une flèche dans les dialogues
    - La flèche apparait uniquement si le texte ne rentre pas dans une seule
    boite de message (qu'il faut le déroulé)
    - La flèche est animée : elle va de haut en bas périodiquement
    - Il est nécessaire de définir la flèche dans les paramètres du script
    (comme la GUI utilisée)

--- src/Unity Pokemon Kit/Assets/UPK/Scripts/Characters/Speak.cs
+++ src/Unity Pokemon Kit/Assets/UPK/Scripts/Characters/Speak.cs
@@ -10,22 +10,38 @@ using System.Collections;
  */
 public class Speak : MonoBehaviour {
 
+	[Header("> Simple settings")]
 	[TextArea(3,10)]
 	public string message = "Un texte";
 	public GUISkin skin;
-	public int lineSize = 25;
-	
-	private float MIN_DISTANCE = 0.6f;
-	private int MAX_ANGLE = 45;
-	private int LINES_AMOUNT = 2;
+
+	[Header("> Advanced settings")]
+	public Texture2D arrowImage;
+	public int lineSize = 56;
+
+	private readonly float MESSAGE_BOX_X_ALIGNMENT = 2.0f; // 2.0 is centered
+	private readonly float MESSAGE_BOX_Y_ALIGNMENT = 1.1f; // 2.0 is centered
+	private readonly string INPUT_KEY = "Submit"; // "Space" or "Numerical Pad Enter" key
+	private readonly int LINES_AMOUNT = 2;
+	private readonly int MAX_ANGLE = 45;
+	private readonly float MIN_DISTANCE = 0.6f;
+	private readonly int ARROW_SHIFT_DELAY = 50;
+	private readonly int ARROW_X_POSITION = 356;
+	private readonly int ARROW_Y_POSITION = 44;
+	private readonly int ARROW_Y_SHIFT = 2;
 
 	private GameObject player; 
 	private PlayerController playerController;
-
+	
 	private Rect messageBox;
 	private string[] lines;
 	private int lineIndex;
 	private bool showText;
+
+	private Rect arrowBox;
+	private int arrowTimer;
+	private bool arrowOnTop;
+	private bool showArrow;
 
 	void Start() {
 		this.player = GameObject.FindWithTag("PlayerOverworld");
@@ -33,20 +49,30 @@ public class Speak : MonoBehaviour {
 			Debug.LogError("Tag 'PlayerOverworld' not set up on player's overworld (or player doesn't exist).");
 			this.enabled = false;
 		}
+
 		this.playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
 		if (this.playerController  == null) {
 			Debug.LogError("Script 'PlayerController' not set up on player's overworld (or tag 'Player' not set up on player).");
 			this.enabled = false;
 		}
-		this.messageBox = new Rect ((Screen.width - this.skin.box.fixedWidth) / 2, (Screen.height - this.skin.box.fixedHeight) / 1.1f, 
+
+		this.messageBox = new Rect ((Screen.width - this.skin.box.fixedWidth) / MESSAGE_BOX_X_ALIGNMENT, 
+		                            (Screen.height - this.skin.box.fixedHeight) / MESSAGE_BOX_Y_ALIGNMENT, 
 		                            this.skin.box.fixedWidth, this.skin.box.fixedHeight);
+		this.arrowBox = new Rect (this.messageBox.x + ARROW_X_POSITION, this.messageBox.y + ARROW_Y_POSITION, 
+		                          this.arrowImage.width, this.arrowImage.height);
+
 		this.lines = PlayerDialogServices.convertToLines(this.message, this.lineSize);
-		disableDialog();
+		this.lineIndex = 0;
+		this.showText = false;
+		this.arrowTimer = ARROW_SHIFT_DELAY;
+		this.arrowOnTop = true;
+		this.showArrow = false;
 	}
 	
 	void Update() {
 		if (canSpeak()) {
-			if (Input.GetButtonDown("Submit")) { // "Space" or "Numerical Pad Enter" key
+			if (Input.GetButtonDown(INPUT_KEY)) {
 				lookInTheEyes();
 				if (this.showText) {
 					this.lineIndex += this.LINES_AMOUNT;
@@ -64,17 +90,23 @@ public class Speak : MonoBehaviour {
 		if (showText) {
 			GUI.skin = this.skin;
 			GUI.Box(this.messageBox, PlayerDialogServices.getWrappedLines(this.lines, this.LINES_AMOUNT, this.lineIndex));
+			shiftTheArrow();
 		}
 	}
 
 	private void enableDialog() {
 		this.showText = true;
+		this.arrowTimer = ARROW_SHIFT_DELAY;
+		this.arrowOnTop = true;
+		this.showArrow = (this.lines.Length > LINES_AMOUNT);
 		this.playerController.enabled = false;
 	}
 
 	private void disableDialog() {
 		this.showText = false;
 		this.lineIndex = 0;
+		this.arrowTimer = ARROW_SHIFT_DELAY;
+		this.showArrow = false;
 		this.playerController.enabled = true;
 	}
 
@@ -90,4 +122,15 @@ public class Speak : MonoBehaviour {
 		this.player.transform.LookAt(playerTarget);
 	}
 
+	private void shiftTheArrow() {
+		if (this.showArrow) {
+			this.arrowTimer--;
+			if (this.arrowTimer == 0) {
+				this.arrowOnTop = !this.arrowOnTop;
+				this.arrowBox.y = this.messageBox.y + ARROW_Y_POSITION + ((this.arrowOnTop) ? 0 : ARROW_Y_SHIFT);
+				this.arrowTimer = ARROW_SHIFT_DELAY;
+			}
+			GUI.DrawTexture (this.arrowBox, this.arrowImage, ScaleMode.ScaleToFit, true, 1.0f);
+		}
+	}
 }
